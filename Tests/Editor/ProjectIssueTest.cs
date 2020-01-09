39dea9e876850744d8338414119718e95c90f3ff
Marco Trivellato <marcot@unity3d.com>
2020-01-09 16:37:51 +0000

    Add ProjectIssue constructors

--- Tests/Editor/ProjectIssueTest.cs
+++ Tests/Editor/ProjectIssueTest.cs
@@ -8,10 +8,11 @@ namespace UnityEditor.ProjectAuditor.EditorTests
 		[Test]
 		public void UninitializedIssueTestPasses()
 		{
-			var uninitialised = new ProjectIssue();
+			var uninitialised = new ProjectIssue(new ProblemDescriptor{}, "dummy issue", IssueCategory.ApiCalls);
 			Assert.AreEqual(string.Empty, uninitialised.filename);
 			Assert.AreEqual(string.Empty, uninitialised.relativePath);
 			Assert.AreEqual(string.Empty, uninitialised.callingMethod);
+			Assert.AreEqual(string.Empty, uninitialised.name);
 			Assert.AreEqual(string.Empty, uninitialised.name);
 		}
 	}	
